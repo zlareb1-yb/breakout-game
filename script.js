e9bc4fa6e1a972f25fca6903ba81fb76005dbbde
zlareb1 <zlareb1@gmail.com>
2020-05-02 21:57:43 +0530

    Added Game Over State

--- script.js
+++ script.js
@@ -54,8 +54,16 @@ function draw() {
 	drawBall();
 	drawPaddle();
 
-	if((y + dy) < ballRadius || (y + dy) > canvas.height-ballRadius) {
+	if((y + dy) < ballRadius) {
 		dy = -dy
+	} else if((y + dy) > canvas.height-ballRadius) {
+		if(x > paddleX && x < paddleX + paddleWidth) {
+			dy = -dy;
+		}
+		else {
+			alert("GAME OVER")
+			document.location.reload();	
+		}
 	}
 
 	if((x + dx) < ballRadius || (x + dx) > canvas.width-ballRadius) {
