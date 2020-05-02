4e483198134507a11c9ef53b9959eaf13a4ba597
zlareb1 <zlareb1@gmail.com>
2020-05-02 21:51:25 +0530

    Added Paddle and left, right keyboard keys handlers

--- script.js
+++ script.js
@@ -2,15 +2,36 @@ let canvas = document.getElementById('myCanvas')
 let ctx = canvas.getContext('2d')
 
 
-setInterval(draw, 10);
-
-
 let x = canvas.width/2;
 let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
 let ballRadius = 10;
+let paddleHeight = 10;
+let paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth)/2;
+let rightPressed = false;
+let leftPressed = false;
 
+
+document.addEventListener("keydown", keyDownHandler);
+document.addEventListener("keyup", keyUpHandler);
+
+function keyDownHandler(e) {
+	if(e.keyCode == 39){
+		rightPressed = true;
+	} else if(e.keyCode == 37) {
+		leftPressed = true;
+	}
+}
+
+function keyUpHandler(e) {
+	if(e.keyCode == 39){
+		rightPressed = false;
+	} else if(e.keyCode == 37) {
+		leftPressed = false;
+	}
+}
 
 function drawBall() {
 	ctx.beginPath();
@@ -20,9 +41,18 @@ function drawBall() {
 	ctx.closePath();
 }
 
+function drawPaddle() {
+	ctx.beginPath();
+	ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight)
+	ctx.fillStyle = "#0095DD";
+	ctx.fill();
+	ctx.closePath();
+}
+
 function draw() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	drawBall();
+	drawPaddle();
 
 	if((y + dy) < ballRadius || (y + dy) > canvas.height-ballRadius) {
 		dy = -dy
@@ -32,6 +62,14 @@ function draw() {
 		dx = -dx
 	}
 
+	if(rightPressed && paddleX < canvas.width-paddleWidth) {
+		paddleX += 7;
+	} else if(leftPressed && paddleX > 0) {
+		paddleX -= 7;
+	}
+
 	x += dx;
 	y += dy;
 }
+
+setInterval(draw, 10);
