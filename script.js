be7940674d7fb2ed469b4fec3b27551094924c50
zlareb1 <zlareb1@gmail.com>
2020-05-02 21:36:38 +0530

    Added Ball to reflect after hitting boundaries

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 let canvas = document.getElementById('myCanvas')
 let ctx = canvas.getContext('2d')
 
+
 setInterval(draw, 10);
+
 
 let x = canvas.width/2;
 let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
+let ballRadius = 10;
+
 
 function drawBall() {
 	ctx.beginPath();
-	ctx.arc(x, y, 10, 0, Math.PI*2);
+	ctx.arc(x, y, ballRadius, 0, Math.PI*2);
 	ctx.fillStyle = "#0095DD";
 	ctx.fill();
 	ctx.closePath();
@@ -19,6 +23,15 @@ function drawBall() {
 function draw() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	drawBall();
+
+	if((y + dy) < 0 || (y + dy) > canvas.height) {
+		dy = -dy
+	}
+
+	if((x + dx) < 0 || (x + dx) > canvas.width) {
+		dx = -dx
+	}
+
 	x += dx;
 	y += dy;
 }
