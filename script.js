9713d76cff4d569a920ffdd86da526c984f9cd8c
zlareb1 <zlareb1@gmail.com>
2020-05-02 22:38:19 +0530

    To remove Brick on collision with Ball

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ let bricks = [];
 for(let c = 0; c < brickColumnCount; c++) {
 	bricks[c] = [];
 	for(let r = 0; r < brickRowCount; r++) {
-		bricks[c][r] = {x : 0, y: 0};
+		bricks[c][r] = {x : 0, y: 0, status: 1};
 	}
 }
 
@@ -35,17 +35,18 @@ document.addEventListener("keyup", keyUpHandler);
 function drawBricks() {
 	for(let c = 0; c < brickColumnCount; c++) {
 		for(let r = 0; r < brickRowCount; r++) {
-			
-			let brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
-			let brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+			if(bricks[c][r].status == 1) {
+				let brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+				let brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
 
-			bricks[c][r].x = brickX;
-			bricks[c][r].y = brickY;
-			ctx.beginPath();
-			ctx.rect(brickX, brickY, brickWidth, brickHeight);
-			ctx.fillStyle = "#0095DD";
-			ctx.fill();
-			ctx.closePath();
+				bricks[c][r].x = brickX;
+				bricks[c][r].y = brickY;
+				ctx.beginPath();
+				ctx.rect(brickX, brickY, brickWidth, brickHeight);
+				ctx.fillStyle = "#0095DD";
+				ctx.fill();
+				ctx.closePath();
+			}
 		}
 	}
 }
@@ -82,12 +83,27 @@ function drawPaddle() {
 	ctx.closePath();
 }
 
+function collisionDetection() {
+	for(let c = 0; c < brickColumnCount; c++) {
+		for (let r = 0; r < brickRowCount; r++) {
+			let b = bricks[c][r];
+			if(b.status == 1) {
+				if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+					dy = -dy;
+					b.status = 0;
+				}
+			}
+		}
+	}
+}
+
 function draw() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	drawBall();
 	drawPaddle();
 	drawBricks(); 
-
+	collisionDetection();
+	
 	if((y + dy) < ballRadius) {
 		dy = -dy
 	} else if((y + dy) > canvas.height-ballRadius) {
