208091a0f954d096dc9363a0f38936b5cf38a3e4
zlareb1 <zlareb1@gmail.com>
2020-05-02 21:17:08 +0530

    Creating moving ball on Screen with Canvas

---
+++ script.js
@@ -0,0 +1,19 @@
+let canvas = document.getElementById('myCanvas')
+let ctx = canvas.getContext('2d')
+
+setInterval(draw, 10);
+
+let x = canvas.width/2;
+let y = canvas.height - 30;
+let dx = 2;
+let dy = -2;
+
+function draw() {
+	ctx.beginPath();
+	ctx.arc(x, y, 10, 0, Math.PI*2);
+	ctx.fillStyle = "#0095DD";
+	ctx.fill();
+	ctx.closePath();
+	x += dx;
+	y += dy;
+}
